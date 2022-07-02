767a07715a491253453eaa6fdad0e1e063218d93
cyla00 <ikhayam000@protonmail.com>
2022-07-02 22:14:46 +0200

    db connection + login data

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,23 +4,90 @@ require('dotenv').config()
 const express = require('express')
 const os = require('os')
 const app = express()
+const { Client } = require('pg')
+const client = new Client()
+const { v4: uuidv4 } = require('uuid')
+const { createHash } = require('crypto')
 
-app.use(express.json())
+// sha256 hash generator
+function hash(string :string) {
+    return createHash('sha256').update(string).digest('hex')
+}
 
-let bodyContent
+// database connector and builder
+async function db_build() :Promise<void>{
+    try{
+        await client.connect()
+        await client.query(`CREATE TABLE IF NOT EXISTS admin (
+            id VARCHAR(200) UNIQUE NOT NULL PRIMARY KEY,
+            email VARCHAR(100) UNIQUE NOT NULL ,
+            password VARCHAR(200) UNIQUE NOT NULL,
+            created_on TIMESTAMP NOT NULL,
+            last_login TIMESTAMP
+        );`, async (err :any, res :object) :Promise<void> => {
+            if(err){
+                console.log(err.stack)
+                await client.end()
+                return
+            }
+        })
 
-app.post('/api/login', (req:any, res:any) => {
-    bodyContent = req.body
+        const admin_query = `INSERT INTO admin(id, email, password, created_on) VALUES ($1, $2, $3, current_timestamp) ON CONFLICT (email) DO NOTHING;`
+        const values = [uuidv4(), hash(String(process.env.DEV_EMAIL)), hash(String(process.env.DEV_PW))]
+        await client.query(admin_query, values, async (err :any, res :object) :Promise<void> => {
+            if(err){
+                console.log(err.stack)
+                await client.end()
+                return
+            }
+        })
+
+        await client.query(`CREATE TABLE IF NOT EXISTS projects (
+            id VARCHAR(200) UNIQUE NOT NULL PRIMARY KEY,
+            title VARCHAR(100) NOT NULL ,
+            image VARCHAR(20000) NOT NULL,
+            project_url VARCHAR(200),
+            tech_stack VARCHAR[],
+            created_on TIMESTAMP NOT NULL
+        );`, async (err :any, res :object) :Promise<void> => {
+            if(err){
+                console.log(err.stack)
+                await client.end()
+                return
+            }
+            console.log('database connected and populated')
+            await client.end()
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+db_build()
+
+
+// api starting point
+app.use(express.json()) 
+
+app.post('/api/login', (req:any, res:any) :void => {
+    const email :string = req.body.email
+    const password :string = req.body.password
+    // process login data against database
     res.sendStatus(200)
 })
 
-app.post('/api/addProject', (req:any, res:any) => {
-    bodyContent = req.body
+app.post('/api/projects', (req:any, res:any) :void => {
+
     res.sendStatus(200)
 })
 
-app.post('/api/removeProject', (req:any, res:any) => {
-    bodyContent = req.body
+app.post('/api/addProject', (req:any, res:any) :void => {
+    
+    res.sendStatus(200)
+})
+
+app.post('/api/removeProject', (req:any, res:any) :void => {
+    
     res.sendStatus(200)
 })
 
@@ -29,6 +96,6 @@ if(process.env.PR_STATUS === 'production'){
 }
 
 app.listen(process.env.PORT, () => {
-    console.log(`http://${os.hostname()}:${process.env.PORT}`);
+    console.log(`http://${os.hostname()}:${process.env.PORT}`)
 })
 
